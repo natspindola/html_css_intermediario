362444a8df643dd05efde3e662a109278292d65d
natspindola <natspindola@hotmail.com>
2022-02-22 19:04:52 -0300

    canvas

---
+++ canvas/primeiroCanvas.js
@@ -0,0 +1,17 @@
+var meuCanvas = document.getElementById('meuCanvas');
+var ctx = meuCanvas.getContext("2d");
+
+ctx.moveTo(5, 5);
+ctx.lineTo(100, 300);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(75, 40);
+ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+ctx.fill();
