909c2c8669030d8b958db6fbc70d5132bf632bba
natspindola <natspindola@hotmail.com>
2022-02-21 21:31:21 -0300

    arrastando elementos att

--- dragEDrop/arrastandoElementos.js
+++ dragEDrop/arrastandoElementos.js
@@ -1,3 +1,14 @@
 function dragStart(event){
     event.dataTransfer.setData('text', event.target.id);
  }
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+function drop(event){
+   var imageId = event.dataTransfer.getData('text'),
+   image = document.getElementById(imageId);
+
+   event.target.appendChild(image);
+}
