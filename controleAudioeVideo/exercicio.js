47323d28f5ba705ccb3320eca8fbf6390019a3de
natspindola <natspindola@hotmail.com>
2022-02-21 21:01:09 -0300

    exercicios att

--- controleAudioeVideo/exercicio.js
+++ controleAudioeVideo/exercicio.js
@@ -1,6 +1,15 @@
 var myVideo = document.getElementById('myVideo'),
-logo = document.getElementById('logo')[0],
+logo = document.getElementById('logo'),
 adTimer = null;
+
+myVideo.addEventListener('playing', function(){
+    showAds();
+})
+
+myVideo.addEventListener('pause', function(){
+    stopAds();
+})
+
 
 function showAds(){
     stopAds();
@@ -14,4 +23,19 @@ function showAds(){
 
 function stopAds(){
     clearInterval(adTimer);
+}
+
+function setVideoTime(position){
+    myVideo.currentTime = position;
+    myVideo.play();
+}
+
+function setSpeed(direction){
+    if(direction === 'up'){
+        myVideo.playbackRate += 0.5;
+    } else if(direction === 'down'){
+        myVideo.playbackRate -= 0.25;
+    } else{
+        myVideo.playbackRate = 1;
+    }
 }
